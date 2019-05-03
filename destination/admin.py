92451d50af65b655a8707d396bb7feb81d931353
crndolo <ndoloraphael@gmail.com>
2019-05-03 11:35:43 +0300

    Hotel model is created. This model holds information about all hotels in a specific Destination. The model is also registered in admin.py as is the destination model.

--- destination/admin.py
+++ destination/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from destination.models import Hotel
 from destination.models import Destination
 
-# Register your models here.
 
+# Register your models here.
+admin.site.register(Hotel)
 admin.site.register(Destination)
--- destination/models.py
+++ destination/models.py
@@ -17,3 +17,18 @@ class Destination(models.Model):
 
 		def __str__(self):
 			return self.destination_name
+
+class Hotel(models.Model):
+	hotel_name = models.CharField(_('Hotel'), max_length=255, blank=False, null=False, unique=True)
+	destination = models.OneToOneField(Destination, on_delete=models.CASCADE)
+	description = models.TextField(_('Description'), blank=True, default='')
+
+	created_on = models.DateTimeField(_('Created On'), auto_now_add=True)	
+	updated_on = models.DateTimeField(_('Updated On'), auto_now=True)
+
+	class Meta:
+		verbose_name = "Hotel"
+		verbose_name_plural = "Hotels"
+
+		def __str__(self):
+			return self.hotel_name
