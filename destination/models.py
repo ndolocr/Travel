6130c25a6577b824f7eb539e0ae195d36aedcf6a
crndolo <ndoloraphael@gmail.com>
2019-05-03 15:20:47 +0300

    Adding Featured image field to Hotel model

--- destination/models.py
+++ destination/models.py
@@ -1,8 +1,19 @@
+import os
+import datetime
 from django.db import models
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+def hotel_feature_image_name(instance, filename):
+	#Get file expension
+	ext = filename.split('.')[-1]
+	#Getting current time
+	current_time = datetime.datetime.now()
+
+	filename = '{}.{}'.format(current_time, ext)
+	return os.path.join('hotel_featued_image', filename)
 
 class Destination(models.Model):
 	destination_name = models.CharField(_('Destination'), max_length=255, unique=True, null=False, blank=False)
@@ -22,7 +33,7 @@ class Hotel(models.Model):
 	hotel_name = models.CharField(_('Hotel'), max_length=255, blank=False, null=False, unique=True)
 	destination = models.OneToOneField(Destination, on_delete=models.CASCADE)
 	description = models.TextField(_('Description'), blank=True, default='')
-
+	image = models.ImageField(_('Featured Image - 100px x 100 px'), upload_to=hotel_feature_image_name, blank=False, null=True)
 	created_on = models.DateTimeField(_('Created On'), auto_now_add=True)	
 	updated_on = models.DateTimeField(_('Updated On'), auto_now=True)
 
@@ -32,6 +43,7 @@ class Hotel(models.Model):
 
 	def __str__(self):
 		return self.hotel_name
-
+	'''
 	def __unicode__(self):
 		return self.hotel_name
+	'''
